781cd61a66d2ec19f921e8b84a7d931eb05af107
Ray Brooks <rbrooks@yammer-inc.com>
2014-07-04 22:48:33 +0100

    added io controller, prep for js rewrite of c++

---
+++ io_controller.js
@@ -0,0 +1,14 @@
+// dem Node modules
+var _ = require('underscore');
+var gpio = require('pi-gpio');
+
+var openGpio = function() {
+
+};
+openGpio();
+
+var IOController = function() {
+}
+
+exports.IOController = IOController;
+
