cce969870042a37cd5ffcc0a24968131769144a1
Ray Brooks <rbrooks@yammer-inc.com>
2014-07-22 19:13:28 +0100

    removing cruft

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
-var Server = require("./modules/server").Server;
 var RFIDController = require('./modules/rfid_controller').RFIDController;
 var Arduino = require('./modules/arduino_controller').Arduino;
 var Game = require('./modules/game_controller').Game;
 var Audio = require('./modules/audio_controller').AudioController;
 
-var server = new Server();
 var rfidController = new RFIDController();
 var arduino = new Arduino();
 var game = new Game();
