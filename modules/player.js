b4698710f141b0a98d5de969ebe97c023848d5b7
Ray Brooks <rbrooks@yammer-inc.com>
2014-07-22 19:57:12 +0100

    getTeam method converts side to team

--- modules/player.js
+++ modules/player.js
@@ -2,10 +2,21 @@ var events = require('events');
 var util = require('util');
 var _ = require('underscore');
 
+var SILVER = 0;
+var BLACK = 1; 
+
 var Player = function(id, side) {
 	this.id = id;
 	this.side = side;
 }
+
+Player.prototype.getTeam = function() {
+	if (this.side === SILVER) {
+		return "silver";
+	} else if (this.side === BLACK) {
+		return "black";
+	}
+};
 
 util.inherits(Player, events.EventEmitter);
 
